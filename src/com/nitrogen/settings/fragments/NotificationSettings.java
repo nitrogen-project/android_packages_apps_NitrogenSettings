5bd28ac165263cf3fcea4d75f6889dd8140ef14c
Lars Greiss <kufikugel@googlemail.com>
2013-12-01 14:14:38 +0100

    [2/2] base: Bring back less notification sounds

--- src/com/nitrogen/settings/fragments/NotificationSettings.java
+++ src/com/nitrogen/settings/fragments/NotificationSettings.java
@@ -2,20 +2,28 @@ package com.nitrogen.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
-import com.android.settings.R;
-
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.nitrogen.settings.preferences.Utils;
 
-public class NotificationSettings extends SettingsPreferenceFragment {
+public class NotificationSettings extends SettingsPreferenceFragment
+                        implements OnPreferenceChangeListener {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
+
+    private ListPreference mAnnoyingNotifications;
     private Preference mChargingLeds;
 
     @Override
@@ -38,6 +46,32 @@ public class NotificationSettings extends SettingsPreferenceFragment {
             prefScreen.removePreference(mChargingLeds);
         }
 
+        mAnnoyingNotifications = (ListPreference) findPreference(PREF_LESS_NOTIFICATION_SOUNDS);
+        int notificationThreshold = Settings.System.getInt(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0);
+        mAnnoyingNotifications.setValue(Integer.toString(notificationThreshold));
+        int valueIndex = mAnnoyingNotifications.findIndexOfValue(String.valueOf(notificationThreshold));
+        if (valueIndex > 0) {
+            mAnnoyingNotifications.setSummary(mAnnoyingNotifications.getEntries()[valueIndex]);
+        }
+        mAnnoyingNotifications.setOnPreferenceChangeListener(this);
+
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mAnnoyingNotifications) {
+            String notificationThreshold = (String) newValue;
+            int notificationThresholdValue = Integer.parseInt(notificationThreshold);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, notificationThresholdValue);
+            int notificationThresholdIndex = mAnnoyingNotifications
+                    .findIndexOfValue(notificationThreshold);
+            mAnnoyingNotifications
+                    .setSummary(mAnnoyingNotifications.getEntries()[notificationThresholdIndex]);
+            return true;
+        }
+
+        return false;
     }
 
     @Override
