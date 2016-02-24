b7cdfbc477d0d629f6e93ee25d1f0150728ce093
beanstown106 <nbenis106@gmail.com>
2016-02-23 23:29:23 -0500

    Configurable Keyguard alpha and Keyguard Bouncer alpha [2/2]

--- src/com/pure/settings/fragments/LockScreenSettings.java
+++ src/com/pure/settings/fragments/LockScreenSettings.java
@@ -17,6 +17,7 @@ package com.pure.settings.fragments;
 
 import android.app.Activity;
 import android.app.WallpaperManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
@@ -24,8 +25,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
@@ -35,9 +38,12 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import com.pure.settings.preferences.SeekBarPreference;
 import com.pure.settings.preferences.SystemSettingSwitchPreference;
 
-public class LockScreenSettings extends SettingsPreferenceFragment {
+public class LockScreenSettings extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener  {
+
     public static final int IMAGE_PICK = 1;
     private static final int MY_USER_ID = UserHandle.myUserId();
 
@@ -47,12 +53,17 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
     private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
     private static final String KEY_WALLPAPER_SET = "lockscreen_wallpaper_set";
     private static final String KEY_WALLPAPER_CLEAR = "lockscreen_wallpaper_clear";
+    private static final String KEYGUARD_TORCH = "keyguard_toggle_torch";
+    private static final String LOCKSCREEN_ALPHA = "lockscreen_alpha";
+    private static final String LOCKSCREEN_SECURITY_ALPHA = "lockscreen_security_alpha";
 
     private FingerprintManager mFingerprintManager;
     private Preference mSetWallpaper;
     private Preference mClearWallpaper;
     private SystemSettingSwitchPreference mLsTorch;
     private SystemSettingSwitchPreference mFingerprintVib;
+    private SeekBarPreference mLsAlpha;
+    private SeekBarPreference mLsSecurityAlpha;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,6 +71,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
         addPreferencesFromResource(R.xml.lockscreen_settings);
 
         PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
         final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
 
         PreferenceCategory optionsCategory = (PreferenceCategory) findPreference(LS_OPTIONS_CAT);
@@ -68,7 +80,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
         mSetWallpaper = (Preference) findPreference(KEY_WALLPAPER_SET);
         mClearWallpaper = (Preference) findPreference(KEY_WALLPAPER_CLEAR);
 
-        mLsTorch = (SystemSettingSwitchPreference) prefScreen.findPreference("keyguard_toggle_torch");
+        mLsTorch = (SystemSettingSwitchPreference) findPreference(KEYGUARD_TORCH);
         if (!Utils.deviceSupportsFlashLight(getActivity())) {
             optionsCategory.removePreference(mLsTorch);
         }
@@ -78,6 +90,18 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
         if (!mFingerprintManager.isHardwareDetected()){
             secureCategory.removePreference(mFingerprintVib);
         }
+
+        mLsAlpha = (SeekBarPreference) findPreference(LOCKSCREEN_ALPHA);
+        float alpha = Settings.System.getFloat(resolver,
+                Settings.System.LOCKSCREEN_ALPHA, 0.45f);
+        mLsAlpha.setValue((int)(100 * alpha));
+        mLsAlpha.setOnPreferenceChangeListener(this);
+
+        mLsSecurityAlpha = (SeekBarPreference) findPreference(LOCKSCREEN_SECURITY_ALPHA);
+        float alpha2 = Settings.System.getFloat(resolver,
+                Settings.System.LOCKSCREEN_SECURITY_ALPHA, 0.75f);
+        mLsSecurityAlpha.setValue((int)(100 * alpha2));
+        mLsSecurityAlpha.setOnPreferenceChangeListener(this);
 
         if (!lockPatternUtils.isSecure(MY_USER_ID)) {
             prefScreen.removePreference(secureCategory);
@@ -101,6 +125,23 @@ public class LockScreenSettings extends SettingsPreferenceFragment {
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLsAlpha) {
+            int alpha = (Integer) newValue;
+            Settings.System.putFloat(resolver,
+                    Settings.System.LOCKSCREEN_ALPHA, alpha / 100.0f);
+            return true;
+        } else if (preference == mLsSecurityAlpha) {
+            int alpha2 = (Integer) newValue;
+            Settings.System.putFloat(resolver,
+                    Settings.System.LOCKSCREEN_SECURITY_ALPHA, alpha2 / 100.0f);
+            return true;
+        }
+        return false;
     }
 
     @Override
