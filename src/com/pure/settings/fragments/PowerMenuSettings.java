b03e8c4e19f4de0210b7ed66cdb251179ccb2f39
LorDClockaN <davor@losinj.com>
2016-03-24 16:30:28 -0400

    Powermenu Torch option [2/2] - Ported up by @Mazda-- from Lollipop DirtyUnicorns/android_frameworks_base@fd77071
    - Use CameraManager instead of TorchManager since we no longer use CM's torch fixes/implementation
    
    Credit to @beanstown106 for his commit here
    DirtyUnicorns/android_frameworks_base@49e3cd5
    
    PureNexus Edits:
    *made to work with cm style powermenu
    *also remove preference if device doesnt support flashlight

--- src/com/pure/settings/fragments/PowerMenuSettings.java
+++ src/com/pure/settings/fragments/PowerMenuSettings.java
@@ -28,6 +28,7 @@ import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 import com.android.internal.logging.MetricsLogger;
 
 import com.android.internal.util.purenexus.PowerMenuConstants;
@@ -41,6 +42,7 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
     private SwitchPreference mRebootPref;
     private SwitchPreference mScreenshotPref;
     private SwitchPreference mScreenRecordPref;
+    private SwitchPreference mTorchPref;
     private SwitchPreference mAirplanePref;
     private SwitchPreference mUsersPref;
     private SwitchPreference mSettingsPref;
@@ -79,6 +81,8 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
                 mScreenshotPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
             } else if (action.equals(GLOBAL_ACTION_KEY_SCREENRECORD)) {
                 mScreenRecordPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SCREENRECORD);
+            } else if (action.equals(GLOBAL_ACTION_KEY_TORCH)) {
+                mTorchPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_TORCH);
             } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
                 mAirplanePref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
             } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
@@ -120,6 +124,15 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
 
         if (mScreenRecordPref != null) {
             mScreenRecordPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENRECORD));
+        }
+
+        if (mTorchPref != null) {
+            if (!Utils.deviceSupportsFlashLight(getActivity())) {
+                getPreferenceScreen().removePreference(findPreference(GLOBAL_ACTION_KEY_TORCH));
+                mTorchPref = null;
+            } else {
+                mTorchPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_TORCH));
+            }
         }
 
         if (mAirplanePref != null) {
@@ -187,6 +200,10 @@ public class PowerMenuSettings extends SettingsPreferenceFragment {
         } else if (preference == mScreenRecordPref) {
             value = mScreenRecordPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENRECORD);
+
+        } else if (preference == mTorchPref) {
+            value = mTorchPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_TORCH);
 
         } else if (preference == mAirplanePref) {
             value = mAirplanePref.isChecked();
