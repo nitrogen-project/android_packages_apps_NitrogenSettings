0428dae3598bf68bf5bfdf31f7b6ce161c3b5c67
Sang Tae Park <pastime1971@gmail.com>
2016-10-30 21:19:37 +0800

    Add option to disable scrolling cache [2/2]
    NitrogenOS: improve summary string showing

--- src/com/nitrogen/settings/fragments/MiscSettings.java
+++ src/com/nitrogen/settings/fragments/MiscSettings.java
@@ -13,11 +13,11 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.Preference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
@@ -32,6 +32,11 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+    private static final String SCROLLINGCACHE_DEFAULT = "1";
+
+    private ListPreference mScrollingCachePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,12 +48,28 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         if (!DevelopmentSettings.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
             getPreferenceScreen().removePreference(findPreference(KEY_LOCK_CLOCK));
         }
+
+        mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
+        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+        mScrollingCachePref.setSummary(mScrollingCachePref.getEntry());
+        mScrollingCachePref.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
-    return false;
+        if (preference == mScrollingCachePref) {
+            if (objValue != null) {
+            String ScrollingCache = (String) objValue;
+            SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, ScrollingCache);
+            int ScrollingCacheIndex = mScrollingCachePref
+                    .findIndexOfValue(ScrollingCache);
+            mScrollingCachePref
+                    .setSummary(mScrollingCachePref.getEntries()[ScrollingCacheIndex]);
+            }
+            return true;
+        }
+        return false;
     }
 
     @Override
