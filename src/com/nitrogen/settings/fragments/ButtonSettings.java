e4404a94e04a0ddc82ca0849ce4b325a8eb49590
Steve Kondik <steve@cyngn.com>
2016-11-02 20:47:41 +0800

    Add split screen to button behaviors

--- src/com/nitrogen/settings/fragments/ButtonSettings.java
+++ src/com/nitrogen/settings/fragments/ButtonSettings.java
@@ -68,6 +68,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final int ACTION_LAUNCH_CAMERA = 6;
     private static final int ACTION_SLEEP = 7;
     private static final int ACTION_LAST_APP = 8;
+    private static final int ACTION_SPLIT_SCREEN = 9;
 
     // Masks for checking presence of hardware keys.
     // Must match values in frameworks/base/core/res/res/values/config.xml
@@ -129,14 +130,14 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             int defaultLongPressAction = res.getInteger(
                     com.android.internal.R.integer.config_longPressOnHomeBehavior);
             if (defaultLongPressAction < ACTION_NOTHING ||
-                    defaultLongPressAction > ACTION_LAST_APP) {
+                    defaultLongPressAction > ACTION_SPLIT_SCREEN) {
                 defaultLongPressAction = ACTION_NOTHING;
             }
 
             int defaultDoubleTapAction = res.getInteger(
                     com.android.internal.R.integer.config_doubleTapOnHomeBehavior);
             if (defaultDoubleTapAction < ACTION_NOTHING ||
-                    defaultDoubleTapAction > ACTION_LAST_APP) {
+                    defaultDoubleTapAction > ACTION_SPLIT_SCREEN) {
                 defaultDoubleTapAction = ACTION_NOTHING;
             }
 
