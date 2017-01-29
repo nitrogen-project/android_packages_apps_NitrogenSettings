615aa8f36dc8e96b21b7076173de19f33bdc8895
maxwen <max.weninger@gmail.com>
2017-01-29 08:58:30 +0800

    OmniGears: order header pack names same ordering as in the browse activity

--- src/com/nitrogen/settings/fragments/StatusBarSettings.java
+++ src/com/nitrogen/settings/fragments/StatusBarSettings.java
@@ -30,6 +30,9 @@ import android.util.Log;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
 
 import com.nitrogen.settings.preferences.CustomSeekBarPreference;
 
@@ -143,36 +146,41 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     }
 
     private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
+        String defaultLabel = null;
+        Map<String, String> headerMap = new HashMap<String, String>();
         Intent i = new Intent();
         PackageManager packageManager = getPackageManager();
         i.setAction("org.omnirom.DaylightHeaderPack");
         for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
             String packageName = r.activityInfo.packageName;
-            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
-                values.add(0, packageName);
-            } else {
-                values.add(packageName);
-            }
             String label = r.activityInfo.loadLabel(getPackageManager()).toString();
             if (label == null) {
                 label = r.activityInfo.packageName;
             }
             if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
-                entries.add(0, label);
+                defaultLabel = label;
             } else {
-                entries.add(label);
+                headerMap.put(label, packageName);
             }
         }
         i.setAction("org.omnirom.DaylightHeaderPack1");
         for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
             String packageName = r.activityInfo.packageName;
-            values.add(packageName  + "/" + r.activityInfo.name);
 
             String label = r.activityInfo.loadLabel(getPackageManager()).toString();
             if (label == null) {
                 label = packageName;
-            }
-            entries.add(label);
+             }
+            headerMap.put(label, packageName  + "/" + r.activityInfo.name);
         }
+        List<String> labelList = new ArrayList<String>();
+        labelList.addAll(headerMap.keySet());
+        Collections.sort(labelList);
+        for (String label : labelList) {
+              entries.add(label);
+            values.add(headerMap.get(label));
+        }
+        entries.add(0, defaultLabel);
+        values.add(0, DEFAULT_HEADER_PACKAGE);
     }
 }
