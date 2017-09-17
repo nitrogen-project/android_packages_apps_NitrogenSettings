a494b49d2e1340bb5ea5c0b9920c07275c1cefbc
ezio84 <brabus84@gmail.com>
2017-09-17 19:28:50 +0800

    Battery light customization [2/2]

--- src/com/nitrogen/settings/fragments/NotificationSettings.java
+++ src/com/nitrogen/settings/fragments/NotificationSettings.java
@@ -5,6 +5,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import android.os.Bundle;
 import com.android.settings.R;
 
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 
@@ -15,6 +16,7 @@ import com.nitrogen.settings.preferences.Utils;
 public class NotificationSettings extends SettingsPreferenceFragment {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+    private Preference mChargingLeds;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -28,6 +30,14 @@ public class NotificationSettings extends SettingsPreferenceFragment {
         if (!Utils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(incallVibCategory);
         }
+
+        mChargingLeds = (Preference) findPreference("charging_light");
+        if (mChargingLeds != null
+                && !getResources().getBoolean(
+                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
+            prefScreen.removePreference(mChargingLeds);
+        }
+
     }
 
     @Override
