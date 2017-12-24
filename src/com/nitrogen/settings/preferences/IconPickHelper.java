3488f17de6019e4112a822b058af4bb4c6c9e9b6
xyyx <xyyx@mail.ru>
2017-12-24 09:40:38 +0800

    DUI: fix my bad merging

--- src/com/nitrogen/settings/preferences/IconPickHelper.java
+++ src/com/nitrogen/settings/preferences/IconPickHelper.java
@@ -40,7 +40,7 @@ public class IconPickHelper {
     public static final int REQUEST_PICK_ICON_PACK = 100;
     public static final int REQUEST_PICK_ICON_GALLERY = 101;
     public static final String INTENT_ACTION_EDIT_CLASS = "com.android.settings";
-    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.nitrogen.settings.IconPickerActivity";
+    public static final String INTENT_ACTION_ICON_PICKER_COMPONENT = "com.nitrogen.settings.preferences.IconPickerActivity";
     private static final int REQUEST_CODE_ICON_PACK = 420;
     private static final int REQUEST_CODE_GALLERY = 1337;
     private Activity mParent;
--- src/com/nitrogen/settings/preferences/IconPickerActivity.java
+++ src/com/nitrogen/settings/preferences/IconPickerActivity.java
@@ -110,7 +110,7 @@ public class IconPickerActivity extends Activity implements DialogInterface.OnCa
                 String packageName = info.activityInfo.packageName;
                 Intent intent = new Intent();
                 intent.setClassName("com.android.settings",
-                        "com.nitrogen.settings.IconPackGridActivity");
+                        "com.nitrogen.settings.preferences.IconPackGridActivity");
                 intent.putExtra("icon_package_name", packageName);
                 dialog.dismiss();
                 startActivityForResult(intent, ICON_PACK_ICON_RESULT);
