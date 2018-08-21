a392e7d2b4f6432b2d37868bad8ef4b5bab15e1b
Vol Zhdanov <wight554@gmail.com>
2018-08-21 12:18:00 +0000

    PowerMenuSettings: Hide user related switches when no users added

--- src/com/nitrogen/settings/fragments/PowerMenuSettings.java
+++ src/com/nitrogen/settings/fragments/PowerMenuSettings.java
@@ -45,9 +45,15 @@ import com.nitrogen.settings.preferences.Utils;
 public class PowerMenuSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener {
 
+    private static final String KEY_POWERMENU_LOGOUT = "powermenu_logout";
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String KEY_POWERMENU_USERS = "powermenu_users";
 
+    private SwitchPreference mPowermenuLogout;
     private SwitchPreference mPowermenuTorch;
+    private SwitchPreference mPowermenuUsers;
+
+    private UserManager mUserManager;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -66,14 +72,38 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
+
+        mPowermenuLogout = (SwitchPreference) findPreference(KEY_POWERMENU_LOGOUT);
+        mPowermenuLogout.setOnPreferenceChangeListener(this);
+        mPowermenuUsers = (SwitchPreference) findPreference(KEY_POWERMENU_USERS);
+        mPowermenuUsers.setOnPreferenceChangeListener(this);
+        if (!mUserManager.supportsMultipleUsers()) {
+            prefScreen.removePreference(mPowermenuLogout);
+            prefScreen.removePreference(mPowermenuUsers);
+        } else {
+            mPowermenuLogout.setChecked((Settings.System.getInt(resolver,
+                    Settings.System.POWERMENU_LOGOUT, 0) == 1));
+            mPowermenuUsers.setChecked((Settings.System.getInt(resolver,
+                    Settings.System.POWERMENU_USERS, 0) == 1));
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mPowermenuTorch) {
+        if (preference == mPowermenuLogout) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_LOGOUT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowermenuTorch) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWERMENU_TORCH, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowermenuUsers) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_USERS, value ? 1 : 0);
             return true;
         }
         return false;
