d15ef7d2ceee824418177602ee7ba7ce4a82e186
xyyx <xyyx@mail.ru>
2019-05-19 13:35:59 +0800

    Remove useless features from NOS
    Revert "NitrogenSettings: Implement Aggressive Battery [2/2]"
    
    This reverts commit 57bf41526c267d8e996000a662614e77a1265ab5.
    
    Revert "SmartPixels: Add footer warning"
    
    This reverts commit 40f5120d89606c77dff05fd67db0185de547ffae.
    
    Revert "Smart Pixels [2/2]"
    
    This reverts commit 9c26a14b987cd272137e56457917807d93e30b03.

--- src/com/nitrogen/settings/fragments/MiscSettings.java
+++ src/com/nitrogen/settings/fragments/MiscSettings.java
@@ -40,13 +40,6 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.nitrogen_settings_misc);
 
-        boolean enableSmartPixels = getContext().getResources().
-                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
-        Preference SmartPixels = findPreference("smart_pixels");
-
-        if (!enableSmartPixels){
-            getPreferenceScreen().removePreference(SmartPixels);
-        }
     }
 
     @Override
